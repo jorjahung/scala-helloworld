42a166c26d60c5cdbe96399485cb79d303f2cd22
Jorja Hung <jorjahung@gmail.com>
2014-10-10 16:30:26 +0100

    Added Jetty so we can print hello world

--- src/main/scala/helloworld/HelloWorld.scala
+++ src/main/scala/helloworld/HelloWorld.scala
@@ -1,6 +1,25 @@
 package helloworld
 
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+
+import org.eclipse.jetty.server.{Request, Server}
+import org.eclipse.jetty.server.handler.AbstractHandler
+
+class HelloWorldHandler extends AbstractHandler {
+
+  override def handle(target: String, baseRequest: Request, request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    response.setContentType("text/html;charset=utf-8");
+    response.setStatus(HttpServletResponse.SC_OK);
+    baseRequest.setHandled(true);
+    response.getWriter().println("<h1>Hello World</h1>");
+    response.getWriter().println("<p>Jorja is awesome</p>");
+  }
+}
+
 object HelloWorld extends App {
-  println("Jorja is awesome")
-  println("Hello World")
+  private val server: Server = new Server(8091)
+
+  server.setHandler(new HelloWorldHandler())
+  server.start()
+  server.join()
 }
